7a48941280fb075f6355b8131fb46e0d9597a51e
Michael Hartman <mphartman@me.com>
2018-12-21 15:55:38 -0600

    Update from lastest sample from Auth0

--- src/App.js
+++ src/App.js
@@ -27,6 +27,14 @@ class App extends Component {
     this.props.auth.logout();
   }
 
+  componentDidMount() {
+    const { renewSession } = this.props.auth;
+
+    if (localStorage.getItem('isLoggedIn') === 'true') {
+      renewSession();
+    }
+  }
+
   render() {
     const { isAuthenticated } = this.props.auth;
     const { classes } = this.props;
@@ -63,47 +71,6 @@ class App extends Component {
                 </Toolbar>
               </AppBar>
             </div>
-
-//      <div>
-//        <Navbar fluid>
-//          <Navbar.Header>
-//            <Navbar.Brand>
-//              <a href="#">Auth0 - React</a>
-//            </Navbar.Brand>
-//            <Button
-//              bsStyle="primary"
-//              className="btn-margin"
-//              onClick={this.goTo.bind(this, 'home')}
-//            >
-//              Home
-//            </Button>
-//            {
-//              !isAuthenticated() && (
-//                  <Button
-//                    id="qsLoginBtn"
-//                    bsStyle="primary"
-//                    className="btn-margin"
-//                    onClick={this.login.bind(this)}
-//                  >
-//                    Log In
-//                  </Button>
-//                )
-//            }
-//            {
-//              isAuthenticated() && (
-//                  <Button
-//                    id="qsLogoutBtn"
-//                    bsStyle="primary"
-//                    className="btn-margin"
-//                    onClick={this.logout.bind(this)}
-//                  >
-//                    Log Out
-//                  </Button>
-//                )
-//            }
-//          </Navbar.Header>
-//        </Navbar>
-//      </div>
     );
   }
 }
